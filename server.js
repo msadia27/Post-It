e17c0e0e58b3603b7782ab1ce82d43b9e8661966
Madelane Sadia <madelanecsadia@gmail.com>
2022-11-12 12:23:43 -0500

    html route

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
 const express = require("express");
-const path = require("path");
 const fs = require("fs");
-const notesRoutes = require("./develop/public/notes.html");
-const htmlRoutes = require("./develop/public/index.html");
+const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(__dirname + "public"));
 
 // Use apiRoutes
 require("./routes/apiRoutes")(app);
+//Use HTML Routes
 app.use("/", htmlRoutes);
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`API server now on port ${PORT}!`);
