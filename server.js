6763d3a3f7d04c22bc2c69e80ca12acf9a50ff45
Madelane Sadia <madelanecsadia@gmail.com>
2022-11-12 02:04:41 -0500

    Add Heroku

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-
+const notesRoutes = require("./develop/public/notes.html");
+const htmlRoutes = require("./develop/public/index.html");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -10,11 +11,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Use apiRoutes
-// app.use("/api", apiRoutes);
-// app.use("/", htmlRoutes);
+app.use("/api", apiRoutes);
+app.use("/", htmlRoutes);
 
-// app.listen(PORT, () => {
-//   console.log(`API server now on port ${PORT}!`);
-// });
-// const notesRoutes = require("./develop/public/notes.html");
-// const htmlRoutes = require("./develop/public/index.html");
+app.listen(PORT, () => {
+  console.log(`API server now on port ${PORT}!`);
+});
+
