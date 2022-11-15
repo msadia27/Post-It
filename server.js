75ff20c0bef652d2f05981226cf463d26bb16e0d
Madelane Sadia <madelanecsadia@gmail.com>
2022-11-15 01:14:07 -0500

    moved to root

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "public"));
 
-const notes = require("./Develop/db/db");
+const notes = require("./db/db");
 
 // Use apiRoutes
 //get
@@ -20,20 +20,17 @@ app.get("/api/notes", (req, res) => {
 app.post("/api/notes", (req, res) => {
   const postIt = req.body;
   notes.push(postIt);
-  fs.writeFilySync(
-    path.join(__dirname, "./Develop/db/db.json"),
-    JSON.stringify(notes)
-  );
+  fs.writeFilySync(path.join(__dirname, "./db/db.json"), JSON.stringify(notes));
   res.json(notes);
 });
 
 //Use HTML Routes
 // app.use("/", htmlRoutes);
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+  res.sendFile(path.join(__dirname, "./public/index.html"));
 });
 
 app.listen(PORT, () => {
