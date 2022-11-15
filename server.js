cdb4c190c4bb6b396bd15e2e996c71c8742cad2c
Madelane Sadia <madelanecsadia@gmail.com>
2022-11-14 23:31:59 -0500

    api route

--- server.js
+++ server.js
@@ -8,15 +8,32 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(__dirname + "public"));
 
+const notes = require("./Develop/db/db");
+
 // Use apiRoutes
-require("./routes/apiRoutes")(app);
+//get
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+//post
+app.post("/api/notes", (req, res) => {
+  const postIt = req.body;
+  notes.push(postIt);
+  fs.writeFilySync(
+    path.join(__dirname, "./Develop/db/db.json"),
+    JSON.stringify(notes)
+  );
+  res.json(notes);
+});
+
 //Use HTML Routes
 app.use("/", htmlRoutes);
 app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/notes.html"));
+  res.sendFile(path.join(__dirname, "./Develop/public/notes.html"));
 });
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
 });
 
 app.listen(PORT, () => {
